e3e6ca718782fafc932be97943f619231c1f4f5a
noot1402 <noor.majed@esme.fr>
2022-12-08 17:30:48 +0100

    new branch

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -1,4 +1,4 @@
-# This files contains your custom actions which can be used to run
+"""# This files contains your custom actions which can be used to run
 # custom Python code.
 #
 # See this guide on how to implement these action:
@@ -7,10 +7,7 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+
 #
 #
 # class ActionHelloWorld(Action):
@@ -25,3 +22,17 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+"""
+from typing import Any, Text, Dict, List
+class ActionRestart(Action):
+
+  def name(self) -> Text:
+      return "action_restart"
+
+  async def run(
+      self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
+  ) -> List[Dict[Text, Any]]:
+
+      # custom behavior
+
+      return [...]
