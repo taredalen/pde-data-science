cb44b744a89730268d73fee326f16e9076245267
taredallen <51906844+taredallen@users.noreply.github.com>
2023-03-20 13:02:37 +0100

    update server & rules

--- widget/server.py
+++ widget/server.py
@@ -1,26 +1,10 @@
-
-
-
 # Importing the necessary Libraries
 from flask_cors import cross_origin
 from flask import Flask, render_template, request
-import speech_recognition as sr
+#import speech_recognition as sr
 
 app = Flask(__name__)
-r = sr.Recognizer()
-
-def stt():
-    with sr.Microphone as source2:
-        r.adjust_for_ambient_noise(source2, duration=0.2)
-        audio2  = r.listen(source2)
-
-        text = r.recognize_google(audio2)
-        text = text.lower()
-
-        print(text)
-
-        return text
-
+#r = sr.Recognizer()
 
 
 @app.route('/', methods=['POST', 'GET', 'OPTIONS'])
